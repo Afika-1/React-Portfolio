a57706e3981383048771a2d4f067285d45e2a638
Afika <afikashwashwa@gmail.com>
2023-08-18 15:50:27 +0200

    navbar and form completed styling and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useState } from 'react'
 import './App.css'
-// import backgound from './/assets/background 3.jpg'
 import linkedin from './/assets/linkedin.gif'
 import github from './/assets/github.gif'
 import logo from './/assets/logo.jpg'
-
-
+// import {faPhone, FaEnvelope, faLocation}from 'react-icons/fa' 
 
 function App() {
 
@@ -15,11 +13,6 @@ function App() {
       <div className="contact">
         <div className="overlap-wrapper">
           <div className="overlap">
-
-            <div className="background-image">
-              
-            </div>
-
             <header className='header-class'>
               <nav className='navbar'>
                 <img className="logo" alt="logo Image" src={logo} />
@@ -38,9 +31,6 @@ function App() {
             <div className='title'>
               <h1 className="text-wrapper">Contact <span className="text-wrapper-2">Me</span></h1>
             </div>
-            {/* <img className="fluent-call" alt="Fluent call" src="image.svg" /> */}
-            <div className="background-home" />
-            <div className="div" />
 
             <div id="line"><p className="line"></p></div>
 
@@ -50,28 +40,33 @@ function App() {
 
 
             <div className="contact-content">
-              <div>
-                <div className="text-wrapper-7">+27662398354</div>
-                <div className="text-wrapper-8">Afikashwashwa@gmail.com</div>
-                <div className="text-wrapper-9">Cape Town, South Africa</div>
+
+              <div className="contact-det">
+
+                <div className="contact-container">
+                  <div className="det-div">
+                    <p>+27662398354</p>
+                    <p >Afikashwashwa@gmail.com</p>
+                    <p >Cape Town, South Africa</p>
+                  </div>
+
+                  <div className="icons">
+                    <img className="github" alt="Github link" src={github} />
+                    <img className="linkedin-logo" alt="Linkedin link" src={linkedin} />
+                  </div>
+
+                </div>
               </div>
-              <img className="github" alt="Github" src={github} />
-              <img className="linkedin-logo" alt="Linkedin logo" src={linkedin} />
+
               <form>
                 <input className="form-input" type='text' name='name' placeholder='Full Name' />
                 <input className="form-input" type='email' name='email' placeholder='Email Address' />
                 <input className="form-input" type='text' name='project' placeholder='Project' />
-                <input className="form-input" type='text' name='message' placeholder='Type your message' />
+                <textarea className="form-message" type='text' name='message' placeholder='Type your message' />
                 <div><button className="send-button" type="submit">Send</button></div>
               </form>
 
             </div>
-            {/* <img className="vector" alt="Vector" src="vector.svg" />
-            <img className="mdi-email" alt="Mdi email" src="mdi-email.svg" />
-            <img className="zondicons-location" alt="Zondicons location" src="zondicons-location.svg" />
-            <img className="fluent-call-2" alt="Fluent call" src="fluent-call-20-regular.svg" />
-            <img className="group" alt="Group" src="group.png" /> */}
-
           </div>
       // </div>
     // </div>
