343bd758bdc0a0c9ab92a7fb4455576e9368e1c7
Afika <afikashwashwa@gmail.com>
2023-08-22 16:44:56 +0200

    Set font sixe root and layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,9 @@ export default function About() {
                     I am well equipped with tech knowledge and tools hence proficient in computer programs such as Visual Studio Code, SASAMS And Packet Tracer as examples.
                     <br></br>I have excellent Understanding and skills in languages such as HTML, CSS and JavaScript and upskilling with ReactJS and NodeJS. I currently specialize in Front End Web.</p>
                 <div className='boxes'>
-                    <button>ok</button><button>ok</button><button>ok</button>
+                    <div>ok</div>
+                    <div>ok</div>
+                    <div>ok</div>
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import logo from '../assets/logo.jpg'
 import './navigation.css'
 
+
 export default function Navbar() {
 
     return (
@@ -13,12 +14,14 @@ export default function Navbar() {
                 <ul>
                     <li><a href='#home' className='home'>Home</a></li>
                     <li><a href="#about" className="text-wrapper-4">About</a></li>
-                    <li><a href="#project" className="text-wrapper-5">Project</a></li>
-                    <li><a href="#contact" className="text-wrapper-6">Contact</a></li>
+                    <li><a href="#project" className="text-wrapper-5">Project</a>
+                    </li>
+                    <li>
+                        <a href="#contact" className="text-wrapper-6">Contact</a></li>
                 </ul>
+                    
 
-
-                <button className="btn-cv">Resume</button>
+                <button className="btn-cv"> Resume</button>
             </nav>
         </header>
     );};
