671cda93172bd2ebe707e67e1b000f7ce5dc6311
Afika <afikashwashwa@gmail.com>
2023-08-24 16:08:04 +0200

    Added thr basic content on projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import '../components/projects.css'
 
 export default function Projects() {
-  return (
-    <div className='projects'>
+    return (
+        <div className='projects'>
 
-    <div className='contact-title'>
-        <h1 className="text-wrapper">My <span className="text-wrapper-2">Projects</span></h1>
-      </div>
-      <div id="line"><p className="line"></p></div>
+            <div className='contact-title'>
+                <h1 className="text-wrapper">My <span className="text-wrapper-2">Projects</span></h1>
+            </div>
+            <div id="line"><p className="line"></p></div>
+  
 
 
-
-    </div>
-  )
+        </div>
+    )
 }
 
