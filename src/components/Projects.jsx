45b7f0bb5235e2d7afeb877becfa4850de0d663d
Afika <afikashwashwa@gmail.com>
2023-08-24 18:02:34 +0200

    Started adding content to the code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,36 +18,41 @@ export default function Projects() {
             <div id="line"><p className="line"></p></div>
 
             <div className='project-content'>
-                <div className='icons'></div>
-                <div>
+                <div className='icons'> Icons and other icons</div>
+
+                <div className='image-descr'>
                     <div className='description-icons'>
                         <div className='project-image'><img src={web}/></div>
-                        <div className='description'></div>
+                        <div className='description'><h1>School Website</h1><p>We were tasked to create a website for the school to improve their online presence.  We used HTML, CSS and JS tools to achieve this</p>
+                        <button>Tap to view</button></div>
                     </div>
 
                     <div className='description-icons'>
+                    <div className='description'><h1></h1><p></p><button></button> ef se fws fgs </div>
                         <div className='project-image'><img src={bank}/></div>
-                        <div className='description'></div>
+                       
                     </div>
 
                     <div className='description-icons'>
                         <div className='project-image'><img src={chat}/></div>
-                        <div className='description'></div>
+                        <div className='description'> <h1>gfdgdf</h1><p></p><button></button></div>
                     </div>
 
                     <div className='description-icons'>
+                    <div className='description'> sdfsfds dfs dfsdf <h1></h1><p></p><button></button></div>
                         <div className='project-image'><img src={tax}/></div>
-                        <div className='description'></div>
+                   
                     </div>
 
                     <div className='description-icons'>
                         <div className='project-image'><img src={simple}/></div>
-                        <div className='description'></div>
+                        <div className='description'> sf ds fdsf dsf sdf<h1></h1><p></p><button></button></div>
                     </div>
 
                     <div className='description-icons'>
+                    <div className='description'> <h1></h1><p></p><button></button>dsfgd sg</div>
                         <div className='project-image'><img src={income}/></div>
-                        <div className='description'></div>
+                     
                     </div>
 
                 </div>
