ff51a05377c4dd684bb241bb23e88c87e9a42c33
Afika <afikashwashwa@gmail.com>
2023-08-24 16:40:17 +0200

    added project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import '../components/projects.css'
+import web from '../components/projects/mzamo.png'
+import bank from '../components/projects/bank.png'
+import chat from '../components/projects/ChatApp.png'
+import income from '../components/projects/INCOME.jpg'
+import red from '../components/projects/red.png'
+import tax from '../components/projects/tax-calculator.jpg'
+import simple from '../components/projects/simple.png'
 
 export default function Projects() {
     return (
@@ -9,10 +16,41 @@ export default function Projects() {
                 <h1 className="text-wrapper">My <span className="text-wrapper-2">Projects</span></h1>
             </div>
             <div id="line"><p className="line"></p></div>
-  
-            <div>
-            <div>
-            </div>
+
+            <div className='project-content'>
+                <div className='icons'></div>
+                <div>
+                    <div className='description-icons'>
+                        <div className='project-image'><img src={web}/></div>
+                        <div className='description'></div>
+                    </div>
+
+                    <div className='description-icons'>
+                        <div className='project-image'><img src={bank}/></div>
+                        <div className='description'></div>
+                    </div>
+
+                    <div className='description-icons'>
+                        <div className='project-image'><img src={chat}/></div>
+                        <div className='description'></div>
+                    </div>
+
+                    <div className='description-icons'>
+                        <div className='project-image'><img src={tax}/></div>
+                        <div className='description'></div>
+                    </div>
+
+                    <div className='description-icons'>
+                        <div className='project-image'><img src={simple}/></div>
+                        <div className='description'></div>
+                    </div>
+
+                    <div className='description-icons'>
+                        <div className='project-image'><img src={income}/></div>
+                        <div className='description'></div>
+                    </div>
+
+                </div>
             </div>
 
 
