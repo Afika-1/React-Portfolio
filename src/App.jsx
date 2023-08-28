6fabb7351bb4a8f1dff84d28ada216cb291e3223
Afika <afikashwashwa@gmail.com>
2023-08-28 15:55:56 +0200

    added new particles json file to add particle background

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
-// import {faPhone, FaEnvelope, faLocation}from 'react-icons/fa' 
-
+import Backgound from "./components/Background";
 function App() {
 
   return (
@@ -16,7 +15,7 @@ function App() {
 
       <Navbar />
       <Portfolio />
-
+      <Backgound />
       <About />
       <Projects />
       <Education />
---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Particles from "react-tsparticles";
+import backgoundConfig from './Background-config'
+
+export default function Background(){
+  return (
+
+    <Particles params={backgoundConfig}>
+    
+    </Particles>
+  )
+}
+
