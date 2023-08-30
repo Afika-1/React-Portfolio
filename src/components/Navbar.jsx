67a524ddd30e3b9c57e408f4179186f9fe9dd081
Afika <afikashwashwa@gmail.com>
2023-08-30 17:43:36 +0200

    navabar is now responsive but the links are inline instead of stacking

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,49 +28,8 @@
 
 
 
-// import React, { useState } from 'react';
-// import ReactDOM from 'react-dom/client';
-// import logo from '../assets/logo.jpg';
-// import './navigation.css';
-
-// export default function Navbar() {
-//     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-//     const toggleMobileMenu = () => {
-//         setMobileMenuOpen(!mobileMenuOpen);
-//     };
-
-//     return (
-//         <header className='header-class'>
-//             <nav className={`navbar ${mobileMenuOpen ? 'mobile-menu-open' : ''}`}>
-//                 <img className="logo" alt="logo Image" src={logo} />
-                
-//                 {/* Mobile Menu Toggle Button */}
-//                 <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
-//                     <div className="bar"></div>
-//                     <div className="bar"></div>
-//                     <div className="bar"></div>
-//                 </button>
-
-//                 {/* Navigation Items */}
-//                 <ul className={`nav-items ${mobileMenuOpen ? 'active' : ''}`}>
-//                     <li><a href='#home' className='home'>Home</a></li>
-//                     <li><a href="#about" className="text-wrapper-4">About</a></li>
-//                     <li><a href="#project" className="text-wrapper-5">Project</a></li>
-//                     <li><a href="#contact" className="text-wrapper-6">Contact</a></li>
-//                 </ul>
-
-//                 <button className="btn-cv"> Resume</button>
-//             </nav>
-//         </header>
-//     );
-// }
-
-
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faHome, faInfoCircle, faProjectDiagram, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/logo.jpg';
 import './navigation.css';
 
@@ -83,20 +42,25 @@ export default function Navbar() {
 
     return (
         <header className='header-class'>
-            <nav className='navbar'>
+            <nav className={`navbar ${mobileMenuOpen ? 'mobile-menu-open' : ''}`}>
                 <img className="logo" alt="logo Image" src={logo} />
+                
+                {/* Mobile Menu Toggle Button */}
+                <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </button>
 
-                <ul className={mobileMenuOpen ? 'nav-list mobile-open' : 'nav-list'}>
-                    <li><a href='#home'><FontAwesomeIcon icon={faHome} /></a></li>
-                    <li><a href="#about"><FontAwesomeIcon icon={faInfoCircle} /></a></li>
-                    <li><a href="#project"><FontAwesomeIcon icon={faProjectDiagram} /></a></li>
-                    <li><a href="#contact"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+                {/* Navigation Items */}
+                <ul className={`nav-items ${mobileMenuOpen ? 'active' : ''}`}>
+                    <li><a href='#home' className='home'>Home</a></li>
+                    <li><a href="#about" className="text-wrapper-4">About</a></li>
+                    <li><a href="#project" className="text-wrapper-5">Project</a></li>
+                    <li><a href="#contact" className="text-wrapper-6">Contact</a></li>
                 </ul>
 
-                <button className="btn-cv">Resume</button>
-                <div className="mobile-menu" onClick={toggleMobileMenu}>
-                    <FontAwesomeIcon icon={faBars} />
-                </div>
+                <button className="btn-cv"> Resume</button>
             </nav>
         </header>
     );
