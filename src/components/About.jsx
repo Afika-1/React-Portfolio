89cee64586cf641c0624ec9a25661a1bb88520e3
Afika <afikashwashwa@gmail.com>
2023-09-01 15:46:32 +0200

    restyled my about page and navigation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,52 +3,53 @@ import './styling/about.css'
 
 export default function About() {
     return (
-        <div>
 
-            <div className='about'>
+        <div className='about'>
 
-                <div className='title'>
-                    <h1 className="text-wrapper">About <span className="text-wrapper-2">Me</span></h1>
-                </div>
-                <div id="line"><p className="line"></p></div>
+            <div className='title'>
+                <h1 className="text-wrapper">About <span className="text-wrapper-2">Me</span></h1>
+            </div>
+            <div id="line"><p className="line"></p></div>
 
-                <div className='about-content'>
+            <div className='about-content'>
 
-                    <div className='qualification'>
+                <div className='qualification'>
 
-                        <div className='sub'>
-                            <p>Qualification </p>
-                            <p>Post</p>
-                        </div>
+                    <div className='sub'>
+                        <p className='p1'>Qualification  </p>
 
-                        <div>
-                            <p>: Dip IT</p>
-                            <p>: Software Developer</p>
-                        </div>
+                        <p className='p1'>Post </p>
+
                     </div>
 
+                    <div className='sub'>
+                        <p>: Dip IT</p>
+                        <p>: Software Developer</p>
+                    </div>
+                </div>
 
 
-                    <p>I am a Graduate with a Diploma: IT (CN) that branched out to Software Development out of interest and passion for Mobile App Development  .
-                        I am well equipped with tech knowledge and tools hence proficient in computer programs such as Visual Studio Code, SASAMS And Packet Tracer as examples.
-                        <br></br>I have excellent Understanding and skills in languages such as HTML, CSS and JavaScript and upskilling with ReactJS and NodeJS. I currently specialize in Front End Web.</p>
-                    <div className='boxes'>
-                        <div>
-                            <p>1+</p>
-                            <p>Years of Experience</p>
-                        </div>
-                        <div>
-                            <p>1+</p>
-                            <p>Projects Completed</p>
-                        </div>
-                        <div>
-                            <p>1+</p>
-                            <p>Tap to view My Education</p>
-                        </div>
+
+                <p>I am a Graduate with a Diploma: IT (CN) that branched out to Software Development out of interest and passion for Mobile App Development  .
+                    I am well equipped with tech knowledge and tools hence proficient in computer programs such as Visual Studio Code, SASAMS And Packet Tracer as examples.
+                    <br></br>I have excellent Understanding and skills in languages such as HTML, CSS and JavaScript and upskilling with ReactJS and NodeJS. I currently specialize in Front End Web.</p>
+                <div className='boxes'>
+                    <div>
+                        <p>1+</p>
+                        <p>Years of Experience</p>
+                    </div>
+                    <div>
+                        <p>1+</p>
+                        <p>Projects Completed</p>
+                    </div>
+                    <div>
+                        <p>1+</p>
+                        <p>Tap to view My Education</p>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
