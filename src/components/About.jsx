addb1f8b368152841846500c62762a3e11679b70
Afika <afikashwashwa@gmail.com>
2023-09-11 09:58:29 +0200

    Revert "....."
    This reverts commit d97f3295b57e6bb2b74619ff9bf7734edaa7490c.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,6 +28,8 @@ export default function About() {
                     </div>
                 </div>
 
+
+
                 <p>I am a Graduate with a Diploma: IT (CN) that branched out to Software Development out of interest and passion for Mobile App Development  .
                     I am well equipped with tech knowledge and tools hence proficient in computer programs such as Visual Studio Code, SASAMS And Packet Tracer as examples.
                     <br></br>I have excellent Understanding and skills in languages such as HTML, CSS and JavaScript and upskilling with ReactJS and NodeJS. I currently specialize in Front End Web.</p>
